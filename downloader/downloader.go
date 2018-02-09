a488a8cca01b9cbebe91ecef4d301b95fd6dc195
eager7 <panchagntao@gmail.com>
2018-02-09 14:30:34 +0800

    1. add downloader files

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -1,5 +1,18 @@
 package downloader
-import "../base"
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"net/http"
+	"reflect"
+	"sync"
+
+	"../base"
+	mdw "../middleware"
+)
+
+var downloaderGenerator IdGenerator = NewIdGenerator()
 
 type PageDownloader interface {
 	Id() uint32
@@ -15,4 +28,109 @@ type PageDownloaderPool interface {
 	Return(dl PageDownloader) error
 	Total() uint32
 	Used() uint32
+}
+
+type myIdGenerator struct {
+	sn uint32
+	ended bool
+	mutex sync.Mutex
+}
+
+type myPageDownloader struct {
+	httpClient http.Client
+	id uint32
+}
+
+func NewIdGenerator() IdGenerator {
+	return &myIdGenerator{}
+}
+
+func (gen *myIdGenerator) GetUint32() uint32 {
+	gen.mutex.Lock()
+	defer gen.mutex.Unlock()
+	if gen.ended {
+		defer func() {gen.ended = false}()
+		gen.sn = 0
+		return gen.sn
+	}
+	id := gen.sn
+	if id < math.MaxUint32 {
+		gen.sn ++
+	} else {
+		gen.ended = true
+	}
+	return id
+}
+
+func genDownloaderId() uint32 {
+	return downloaderGenerator.GetUint32()
+}
+
+func NewPageDownloader(client *http.Client) PageDownloader {
+	id := genDownloaderId()
+	if client == nil {
+		client = &http.Client{}
+	}
+	return &myPageDownloader{
+		id:id,
+		httpClient:*client,
+	}
+}
+
+func (dl *myPageDownloader) Id() uint32 {
+	return dl.id
+}
+
+func (dl *myPageDownloader) Download(req base.Request) (*base.Response, error) {
+	httpReq := req.HttpReq()
+	httpResp, err := dl.httpClient.Do(httpReq)
+	if err != nil {
+		return nil, err
+	}
+	return base.NewResponse(httpResp, req.Depth()), nil
+}
+
+type myDownloaderPool struct {
+	pool mdw.Pool
+	etype reflect.Type
+}
+
+type GenPageDownloader func() PageDownloader
+
+func NewPageDownloaderPool(total uint32, gen GenPageDownloader) (PageDownloaderPool, error) {
+	etype := reflect.TypeOf(gen())
+	genEntity := func() mdw.Entity {
+		return gen()
+	}
+	pool ,err := mdw.NewPool(total, etype, genEntity)
+	if err != nil {
+		return nil, err
+	}
+	dlpool := &myDownloaderPool{pool, etype}
+	return dlpool, nil
+}
+
+func (dlpool *myDownloaderPool) Take() (PageDownloader, error) {
+	entity, err := dlpool.pool.Take()
+	if err != nil {
+		return nil, err
+	}
+	dl, ok := entity.(PageDownloader)
+	if !ok {
+		errMsg := fmt.Sprintf("The type of entity is Not %s\n", dlpool.etype)
+		panic(errors.New(errMsg))
+	}
+	return dl, nil
+}
+
+func (dlpool *myDownloaderPool) Return(dl PageDownloader) error {
+	return dlpool.pool.Return(dl)
+}
+
+func (dlpool *myDownloaderPool) Total() uint32 {
+	return dlpool.pool.Total()
+}
+
+func (dlpool *myDownloaderPool) Used() uint32 {
+	return dlpool.pool.Used()
 }
--- middleware/stopsign.go
+++ middleware/stopsign.go
@@ -1,10 +1,79 @@
 package middleware
 
+import (
+	"fmt"
+	"sync"
+)
+
 type StopSign interface {
 	Sign() bool
 	Signed() bool
 	Reset()
 	Deal(code string)
 	DealCount() uint32
+	DealTotal() uint32
 	Summary() string
+}
+
+type myStopSign struct {
+	signed bool
+	rwmutex sync.RWMutex
+	dealCountMap map[string]uint32
+}
+
+func NewStopSign() StopSign {
+	ss := &myStopSign{
+		dealCountMap:make(map[string]uint32),
+	}
+	return ss
+}
+
+func (ss *myStopSign) Sign() bool {
+	ss.rwmutex.Lock()
+	defer ss.rwmutex.Unlock()
+	if ss.signed {
+		return false
+	}
+	ss.signed = true
+	return true
+}
+
+func (ss *myStopSign) Signed() bool {
+	return ss.signed
+}
+
+func (ss *myStopSign) Deal (code string) {
+	ss.rwmutex.Lock()
+	defer ss.rwmutex.Unlock()
+	if !ss.signed {
+		return
+	}
+	if _, ok := ss.dealCountMap[code]; !ok {
+		ss.dealCountMap[code] = 1
+	} else {
+		ss.dealCountMap[code] += 1
+	}
+}
+
+func (ss *myStopSign) Reset() {
+	ss.rwmutex.Lock()
+	defer ss.rwmutex.Unlock()
+	ss.signed = false
+	ss.dealCountMap = make(map[string]uint32)
+}
+
+func (ss *myStopSign)DealCount()uint32 {
+	ss.rwmutex.Lock()
+	defer ss.rwmutex.Unlock()
+	return uint32(len(ss.dealCountMap))
+}
+
+func (ss *myStopSign)DealTotal() uint32 {
+	ss.rwmutex.Lock()
+	defer ss.rwmutex.Unlock()
+	return uint32(len(ss.dealCountMap))
+}
+
+func (ss *myStopSign) Summary()string {
+	return fmt.Sprintf("signed:%d, total:%d\n", ss.signed, len(ss.dealCountMap))
 }
